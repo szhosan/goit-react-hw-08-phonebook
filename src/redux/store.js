c8d70165c7bf6399f2eff0b1a67467f08b09beda
@s_zhsn <s.zhosan@gmail.com>
2022-05-16 21:00:03 +0300

    code-edit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,4 @@
-import {
-  configureStore,
-  combineReducers,
-  getDefaultMiddleware,
-} from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
